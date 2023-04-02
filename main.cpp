32132f1b2125d4d97711a2a35e58e2556851f5c7
Gabs <gab.blond@hotmail.com>
2023-04-02 14:34:38 +0200

    Display function

--- main.cpp
+++ main.cpp
@@ -27,10 +27,31 @@ int calculeExpression(string expression, int x)
 
 }
 
+void calculerSerie(QSplineSeries * series, int minX, int maxX, string fonction, int &minY, int &maxY)
+{
+    for(int i = minX; i < maxX; i++)
+    {
+        int res = calculeExpression(fonction, i);
+        series->append(i, res);
+        if(res < minY)
+        {
+            minY = res;
+        }
+        if (res > maxY)
+        {
+            maxY = res;
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     string fontion = "(x+2)/3";
+    int minX = -10;
+    int maxX = 50;
+    int minY = 1000;
+    int maxY = -1000;
     /*
     // Création des boutons pour ajouter/supprimer des points
     QPushButton *addButton = new QPushButton("Ajouter un point");
@@ -53,19 +74,21 @@ int main(int argc, char *argv[])
     QLabel *labelFonction = new QLabel();
     labelFonction->setText("f(x) = ");
     QLineEdit *lineEditFonction = new QLineEdit();
-    lineEditFonction->setText("x^2");
+    lineEditFonction->setText(QString(fontion.c_str()));
     lineEditFonction->setMaximumWidth(200);
+
+
 
     QLabel *labelMin = new QLabel();
     labelMin->setText("Min x");
     QLineEdit *lineEditMin = new QLineEdit();
-    lineEditMin->setText("0");
+    lineEditMin->setText(QString::number(minX));
     lineEditMin->setMaximumWidth(100);
 
     QLabel *labelMax = new QLabel();
     labelMax->setText("Max x");
     QLineEdit *lineEditMax = new QLineEdit();
-    lineEditMax->setText("20");
+    lineEditMax->setText(QString::number(maxX));
     lineEditMax->setMaximumWidth(100);
 
     // Création du layout pour les boutons
@@ -81,28 +104,33 @@ int main(int argc, char *argv[])
     buttonLayout->addStretch();
 
 
-    cout << calculeExpression("(x+2)/3",1) << endl;
+
 
     // Création de la série de données pour la courbe
     QtCharts::QSplineSeries *series = new QtCharts::QSplineSeries();
-    series->append(0, 6);
-    series->append(2, 4);
-    series->append(3, 8);
-    series->append(7, 4);
-    series->append(10, 5);
+    calculerSerie(series, minX, maxX, fontion, minY, maxY);
+    cout << minY << " " << maxY << endl;
+
+    QtCharts::QSplineSeries *series2 = new QtCharts::QSplineSeries();
+    series2->append(0, 3);
+    series2->append(2, 7);
+    series2->append(3, 8);
+    series2->append(7, 2);
+    series2->append(10, 1);
 
     // Création du graphique et ajout de la série de données
     QtCharts::QChart *chart = new QtCharts::QChart();
     chart->addSeries(series);
+    chart->addSeries(series2);
     chart->createDefaultAxes();
 
     QValueAxis *axisX = qobject_cast<QValueAxis *>(chart->axisX());
     if (axisX)
-        axisX->setRange(0, series->count()*2);
+        axisX->setRange(minX, maxX);
 
     QValueAxis *axisY = qobject_cast<QValueAxis *>(chart->axisY());
     if (axisY)
-        axisY->setRange(0, 10);
+        axisY->setRange(minY, maxY);
 
     // Création de la vue pour le graphique
     QtCharts::QChartView *chartView = new QtCharts::QChartView(chart);
